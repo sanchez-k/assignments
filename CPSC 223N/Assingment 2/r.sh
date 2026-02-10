b626c4feb6592d28a0a3a124596b13273b60f52b
Kassandra <k.sanchez@csu.fullerton.edu>
2026-02-10 13:41:53 -0800

    Copy and pasted the bash file from assignment 1. Haven't configured this file to this program. Will do it later.

---
+++ CPSC 223N/Assingment 2/r.sh
@@ -0,0 +1,34 @@
+# Author: Kassandra Sanchez
+# Email:  k.sanchez@csu.fullerton.edu
+# Cwid:  884962788
+# Assignment:  2
+# Program:  Racing Ball
+# Due:  February 22, 2026 @ 11:59pm
+# Course:  CPSC223N
+# Languages: C# & Bash
+
+#!/bin/bash
+
+echo First remove old binary files
+rm *.dll
+rm *.exe
+
+echo View the list of source files
+ls -l
+
+echo Compile logic1.cs to create the file: logic1.dll
+mcs -target:library -out:logic1.dll logic1.cs
+
+echo Compile metric1.cs to create the file: metric1.dll
+mcs -target:library -r:System.Drawing.dll -r:System.Windows.Forms.dll -r:logic1.dll -out:metric1.dll metric1.cs
+
+echo Compile driver1.cs and link the two previously created dll files to create an executable file. 
+mcs -r:System -r:System.Windows.Forms -r:metric1.dll -r:logic1.dll -out:driver1.exe driver1.cs
+
+echo View the list of files in the current folder
+ls -l
+
+echo Run the Assignment 1 program.
+./driver1.exe
+
+echo The script has terminated.
