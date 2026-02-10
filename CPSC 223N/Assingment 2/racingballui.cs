22f86c391e853fde69be2a884476796bc4e8684c
Kassandra <k.sanchez@csu.fullerton.edu>
2026-02-10 13:47:13 -0800

    Wrote the base layout for the file

---
+++ CPSC 223N/Assingment 2/racingballui.cs
@@ -0,0 +1,21 @@
+/*
+    Author: Kassandra Sanchez
+    Email:  k.sanchez@csu.fullerton.edu
+    Cwid:  884962788
+    Assignment:  2
+    Program:  Racing Ball
+    Due:  February 22, 2026 @ 11:59pm
+    Course:  CPSC223N
+    Languages: C# & Bash
+*/
+
+// wait do i need system for this case?
+using System;
+using Windows.System.Forms;
+using System.Drawing;
+// i think i might need this
+using System.Timers;
+
+public class RacingBall : Form {
+    //
+}
