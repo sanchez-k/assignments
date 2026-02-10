b85138ca43f2f1b001ae739e45e1db398b0852bb
Kassandra <k.sanchez@csu.fullerton.edu>
2026-02-10 13:36:30 -0800

    Wrote the driver file for the program

---
+++ CPSC 223N/Assingment 2/racingball.cs
@@ -0,0 +1,22 @@
+/*
+    Author: Kassandra Sanchez
+    Email:  k.sanchez@csu.fullerton.edu
+    Cwid:  884962788
+    Assignment:  2
+    Program:  Racing Ball
+    Due:  February 22, 2026 @ 11:59pm
+    Course:  CPSC223N
+    Languages: C# & Bash
+*/
+
+using System;
+using System.Windows.Form;
+
+public class RacingBallMain {
+    static void Main(string[] args) {
+        System.Console.WriteLine("Welcome to the Main method of the Racing Ball program.");
+        RacingBall userinterface = new RacingBall();
+        Application.Run(userinterface);
+        System.Console.WriteLine("blub");
+    }
+}
