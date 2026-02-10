1cd5cd75b15467d75de4314c77c04737f7bade1c
Kassandra <k.sanchez@csu.fullerton.edu>
2026-02-10 14:44:28 -0800

    Made it so the bash file compiles the other 2 files into 1 executable file

--- CPSC 223N/Assingment 2/r.sh
+++ CPSC 223N/Assingment 2/r.sh
@@ -16,19 +16,16 @@ rm *.exe
 echo View the list of source files
 ls -l
 
-echo Compile logic1.cs to create the file: logic1.dll
-mcs -target:library -out:logic1.dll logic1.cs
+echo Compile racingballui.cs to create the file: racingballui.dll
+mcs -target:library -r:System.Drawing.dll -r:System.Windows.Forms.dll -out:racingballui.dll racingballui.cs
 
-echo Compile metric1.cs to create the file: metric1.dll
-mcs -target:library -r:System.Drawing.dll -r:System.Windows.Forms.dll -r:logic1.dll -out:metric1.dll metric1.cs
-
-echo Compile driver1.cs and link the two previously created dll files to create an executable file. 
-mcs -r:System -r:System.Windows.Forms -r:metric1.dll -r:logic1.dll -out:driver1.exe driver1.cs
+echo Compile racingball.cs and link the previously created dll file to create an executable file. 
+mcs -r:System -r:System.Windows.Forms -r:racingballui.dll -out:racingballui.exe racingballui.cs
 
 echo View the list of files in the current folder
 ls -l
 
-echo Run the Assignment 1 program.
-./driver1.exe
+echo Run the Assignment 2 program.
+./racingball.exe
 
 echo The script has terminated.
