c240e5da9352169564cc680e4176a6d267176373
Kassandra <k.sanchez@csu.fullerton.edu>
2026-02-23 10:36:15 -0800

    Added and wrote the code for the driver file

---
+++ CPSC 223N/Assignment 3/ricochetball.cs
@@ -0,0 +1,23 @@
+/*
+    Author: Kassandra Sanchez
+    Email:  k.sanchez@csu.fullerton.edu
+    Cwid:  884962788
+    Assignment:  3
+    Program:  Ricochet Ball
+    Due:  March 8, 2026 @ 11:59pm
+    Course:  CPSC223N
+    Languages: C# & Bash
+    Purpose: 
+*/
+
+using System;
+using System.Windows.Forms;
+
+public class RicochetBallMain {
+    static void Main(string[] args) {
+        System.Console.WriteLine("Welcome to the Main method of the Ricochet Ball program.");
+        RicochetBall userinterface = new RicochetBall();
+        Application.Run(userinterface);
+        System.Console.WriteLine("Thank you for using the Ricochet Ball program!");
+    }
+}
