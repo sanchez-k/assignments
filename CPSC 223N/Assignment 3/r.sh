174ffe85199cad6f6afe1967ec18049b34cc41bf
Kassandra <k.sanchez@csu.fullerton.edu>
2026-02-23 10:37:20 -0800

    Wrote the very basic stuff needed for the ui

---
+++ CPSC 223N/Assignment 3/r.sh
@@ -0,0 +1,32 @@
+# Author: Kassandra Sanchez
+# Email:  k.sanchez@csu.fullerton.edu
+# Cwid:  884962788
+# Assignment:  3
+# Program:  Ricochet Ball
+# Due:  March 6, 2026 @ 11:59pm
+# Course:  CPSC223N
+# Languages: C# & Bash
+# Purpose: 
+
+#!/bin/bash
+
+echo First remove old binary files
+rm *.dll
+rm *.exe
+
+echo View the list of source files
+ls -l
+
+echo Compile ricochetballui.cs to create the file: ricochetballui.dll
+mcs -target:library -r:System.Drawing.dll -r:System.Windows.Forms.dll -out:ricochetballui.dll ricochetballui.cs
+
+echo Compile ricochetball.cs and link the previously created dll file to create an executable file. 
+mcs -r:System -r:System.Windows.Forms -r:ricochetballui.dll -out:ricochetball.exe ricochetball.cs
+
+echo View the list of files in the current folder
+ls -l
+
+echo Run the Assignment 2 program.
+./ricochetball.exe
+
+echo The script has terminated.
---
+++ CPSC 223N/Assignment 3/ricochetballui.cs
@@ -0,0 +1,20 @@
+/*
+    Author: Kassandra Sanchez
+    Email:  k.sanchez@csu.fullerton.edu
+    Cwid:  884962788
+    Assignment:  3
+    Program:  Ricochet Ball
+    Due:  March 8, 2026 @ 11:59pm
+    Course:  CPSC223N
+    Languages: C# & Bash
+    Purpose: 
+*/
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Timers;
+
+public class RicochetBall : Form {
+    //
+}
