578d7aaea2fb8c90e703fa1ddde27cb5841d91d1
Kassandra <k.sanchez@csu.fullerton.edu>
2026-05-04 16:43:40 -0700

    Updated the due date

--- CPSC 223N/Assignment 5/ellipse.cs
+++ CPSC 223N/Assignment 5/ellipse.cs
@@ -4,7 +4,7 @@
     Cwid:  884962788
     Assignment:  5
     Program:  Ellipse
-    Due:  April 27, 2026 @ 11:59pm
+    Due:  May 6, 2026 @ 11:59pm
     Course:  CPSC223N-1
     LanguagesTo animate one ball that travels in the shape of an ellipse.
 */
--- CPSC 223N/Assignment 5/r.sh
+++ CPSC 223N/Assignment 5/r.sh
@@ -3,7 +3,7 @@
 # Cwid:  884962788
 # Assignment:  5
 # Program:  Ellipse
-# Due:  April 27, 2026 @ 11:59pm
+# Due:  May 6, 2026 @ 11:59pm
 # Course:  CPSC223N-1
 # Languages: C# & Bash
 # Purpose: To animate one ball that travels in the shape of an ellipse.
